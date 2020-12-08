29f0d507f70d5bd29c268a30ef8ddfc94e8b42ae
Dmitry <dydynovdima6@gmail.com>
2020-12-09 02:16:02 +0300

    mod script

--- lesson1/js/script.js
+++ lesson1/js/script.js
@@ -1,22 +1,21 @@
-let money = prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-let reqCosts = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    price = prompt("Во сколько обойдётся?", "");
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
-    timeData: time,
-    expenses: {
-        costs: reqCosts,
-        howMany: price
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     savings: false
 };
 
-let budgetForOneDay = appData.budget / 30; 
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-alert(budgetForOneDay + " рублей в день");
-console.log(appData);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
