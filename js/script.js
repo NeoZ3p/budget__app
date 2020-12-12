d4f30c619c6f8441e7a27a1d56820546ad274c62
Dmitry <dydynovdima6@gmail.com>
2020-12-12 03:16:39 +0300

    mod script.js

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ let appData = {
     }
 };
 
-for (let keys in appData) {
-    console.log("Наша программа включает в себя данные: " + keys);
+for (let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 };
 console.log(appData);
